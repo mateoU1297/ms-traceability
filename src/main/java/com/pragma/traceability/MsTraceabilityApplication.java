13696f5a73d21d27c0d448e807e3cef8b0389670
mateoU1297 <castro.mateo@correounivalle.edu.co>
2026-03-14 09:38:26 -0500

    Initial commit

---
+++ src/main/java/com/pragma/traceability/MsTraceabilityApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTraceabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTraceabilityApplication.class, args);
+	}
+
+}
